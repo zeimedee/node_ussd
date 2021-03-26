fde06c20f43983892418dadf2fe15417ee6710d7
alexander <zeimedee@gmail.com>
2021-03-26 15:52:12 +0000

    :rocket: ussd app

--- app.js
+++ app.js
@@ -16,19 +16,19 @@ app.post('*', (req,res) => {
     res.set('Content-Type: text/plain');
     console.log(text)
     
-    if(text == ''){
+    if(parseInt(text) == ''){
         //first request
       let response =`CON What will you like to know ? 
         1.my name?
         2.phone number? `;
         res.send(response);
     }
-    else if(text == '1'){
+    else if(parseInt(text) == '1'){
         //logic for first level
        let response = `END my name is alex`;
         res.status(200).send(response);
     }
-    else if(text == '2'){
+    else if(parseInt(text) == '2'){
         let response =  `END my phone number is ${phoneNumber}`;
         res.status(200).send(response);
     }
